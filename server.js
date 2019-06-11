29a18b91cfb76d1353b36cbea181f64b7fa257a4
dusitsiri <dusit.siri12@gmail.com>
2019-06-11 17:07:14 +0700

    edit server.js

--- server.js
+++ server.js
@@ -3,11 +3,30 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const app = express();
 
-const client = new line.Client(config);
 const config = {
     channelAccessToken: process.env.channelAccessToken,
     channelSecret: process.env.channelSecret
 };
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+    console.log(event);
+    if (event.type === 'message' && event.message.type === 'text') {
+        handleMessageEvent(event);
+    } else {
+        return Promise.resolve(null);
+    }
+}
+
+function handleMessageEvent(event) {
+    var msg = {
+        type: 'text',
+        text: 'สวัสดีครับ'
+    };
+
+    return client.replyMessage(event.replyToken, msg);
+}
 
 app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
