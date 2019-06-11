5bc46124a11cbd6bbaec4269ccc7b62f302cf416
dusitsiri <dusit.siri12@gmail.com>
2019-06-11 17:13:21 +0700

    edit server.js

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const config = {
     channelAccessToken: process.env.channelAccessToken,
     channelSecret: process.env.channelSecret
 };
-
 const client = new line.Client(config);
 
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
 function handleEvent(event) {
     console.log(event);
     if (event.type === 'message' && event.message.type === 'text') {
@@ -18,24 +22,15 @@ function handleEvent(event) {
         return Promise.resolve(null);
     }
 }
-
 function handleMessageEvent(event) {
     var msg = {
         type: 'text',
         text: 'สวัสดีครับ'
     };
-
     return client.replyMessage(event.replyToken, msg);
 }
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
-});
-
 app.set('port', (process.env.PORT || 5000));
-
 app.listen(app.get('port'), function () {
     console.log('run at port', app.get('port'));
 });
